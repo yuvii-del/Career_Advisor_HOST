d6e0718b5e4e848f2059e1c36d24bdc04dcdf9f4
yuvi-dell <yuvarajs1029@gmail.com>
2026-04-22 08:55:23 +0530

    Error fx

--- career_advisor/wsgi.py
+++ career_advisor/wsgi.py
@@ -6,6 +6,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Career_Advisor_HOST.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'career_advisor.settings')
 
 application = get_wsgi_application()
